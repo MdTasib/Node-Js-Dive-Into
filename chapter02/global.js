e573a1e69b3d2858ac5d45e284c6d81cec4a0d8c
MdTasib <ohidulalam442@gmail.com>
2021-07-28 21:32:18 +0600

    global

---
+++ chapter02/global.js
@@ -0,0 +1,17 @@
+// runing code browser
+// const a = 10;
+// console.log(window.a);
+
+// function test() {
+//   console.log('Hello Node Js');
+// }
+// console.log(window.test());
+
+// runing code tarminal
+const a = 10;
+console.log(global.a);
+
+function test() {
+  console.log('Hello');
+}
+console.log(global.test());
