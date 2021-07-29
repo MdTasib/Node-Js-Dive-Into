40802ef384761acafdf77799ca84f8ed3f0f4ff1
MdTasib <ohidulalam442@gmail.com>
2021-07-29 22:58:19 +0600

    os module

---
+++ chapter02/os.js
@@ -0,0 +1,11 @@
+const os = require('os');
+
+console.log(os.arch());
+console.log(os.freemem());
+console.log(os.networkInterfaces());
+console.log(os.cpus());
+
+console.log(os.type());
+console.log(os.hostname());
+console.log(os.userInfo());
+console.log(os.homedir());
