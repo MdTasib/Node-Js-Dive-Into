25d438c6cf9f5fd29f51d92d440d8d242f0c6c99
MdTasib <ohidulalam442@gmail.com>
2021-08-02 22:06:42 +0600

    Route

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
+const userRouter = require('./route');
 
 const app = express();
 
-app.get('/about', (req, res) => {
-  res.send('<h1>This is about page</h1>');
-});
+app.use('/user', userRouter);
 
 app.get('/', (req, res) => {
   res.send('<h1>I am listening.</h1>');
-});
-
-app.get('*', (req, res) => {
-  res.send('<h1>404 Page not found</h1>');
 });
 
 app.listen(4000, () => {
