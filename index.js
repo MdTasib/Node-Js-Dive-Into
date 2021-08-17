0a53b268338b48959f3ac24b2468a962fa579cd2
MdTasib <ohidulalam442@gmail.com>
2021-08-17 19:11:35 +0600

    chapter 08

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const express = require('express');
-
-const userRouter = require('./route');
-const postRouter = require('./post');
-
 const app = express();
 
-app.use('/user', userRouter);
-app.use('/posts', postRouter);
-
-// dynamic route
-app.get('/product/:productId/reviews/:reviewId', (req, res) => {
-  const id = req.params;
-  res.send(`Product id - ${id.productId} || Product reviews id - ${id.reviewId}`);
-});
 
 app.get('/', (req, res) => {
   res.send('<h1>I am listening.</h1>');
--- postController.js
+++
@@ -1,8 +0,0 @@
-// Create Separate File for Controllers
-exports.singlePost = (req, res) => {
-  res.send('Create a new post - PostId: ' + req.params.postId);
-};
-
-exports.updatePost = (req, res) => {
-  res.send('update a new post');
-};
