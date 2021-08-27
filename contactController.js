7b4a7cbfd5ea5d12c7846cd6afb15c7da2f23039
MdTasib <ohidulalam442@gmail.com>
2021-08-27 22:24:54 +0600

    Update & delete contact - 7.6

--- contactController.js
+++ contactController.js
@@ -22,3 +22,25 @@ exports.getContactById = (req, res) => {
   const contact = contacts.getContactsById(id);
   res.json(contact);
 };
+
+exports.updateContact = (req, res) => {
+  const { id } = req.params;
+  id = parseInt(id);
+
+  let { name, email, phone } = req.body;
+  const contact = contacts.updateContactById({
+    name,
+    email,
+    phone
+  });
+
+  res.json(contact);
+};
+
+exports.deleteContact = (req, res) => {
+  const { id } = req.params;
+  id = parseInt(id);
+
+  let contact = contacts.deleteContactById(id);
+  res.json(contact);
+};
