3500d7d94923bf4e60337ec72c76c766d5d5726e
MdTasib <ohidulalam442@gmail.com>
2021-08-27 20:02:14 +0600

    contact model - 7.3

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-
 app.get('/', (req, res) => {
-  res.send('<h1>I am listening.</h1>');
+  res.send('<h1>A simple project.</h1>');
 });
 
 app.listen(4000, () => {
