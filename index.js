1ba8e7572295ffdc163deb67742feaea6d40c9c4
MdTasib <ohidulalam442@gmail.com>
2021-09-10 11:57:22 +0600

    8.5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'EJS is an Awesome' });
 });
 
 app.listen(4000, () => {
